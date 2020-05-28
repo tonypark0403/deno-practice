6301019e76b0df3b9d739f301c6fe73f887ae36c
Tony Park <tony.park2@ontario.ca>
2020-05-28 14:16:29 -0400

    added create note

--- routes.ts
+++ routes.ts
@@ -7,3 +7,18 @@ export const getNotes = async (ctx: RouterContext) => {
     const notes = await notesCollection.find();
     ctx.response.body = notes;
 }
+
+export const createNote = async (ctx: RouterContext) => {
+    const { value: { title, body } } = await ctx.request.body();
+    // console.log(body);
+    const note: any = {
+        title,
+        body
+    }
+    const id = await notesCollection.insertOne(note);
+    console.log(id);
+
+    note._id = id;
+    ctx.response.status = 201;
+    ctx.response.body = note;
+}
--- server.ts
+++ server.ts
@@ -1,5 +1,5 @@
 import { Application, Router } from "https://deno.land/x/oak/mod.ts";
-import { getNotes } from "./routes.ts";
+import { getNotes, createNote } from "./routes.ts";
 
 const router = new Router();
 
@@ -8,8 +8,8 @@ router
         ctx.response.body = 'Welcome to notes API';
     })
     .get('/notes', getNotes)
-// .get('/notes/:id', getSingleNote)
-// .post('/notes', createNote)
+    // .get('/notes/:id', getSingleNote)
+    .post('/notes', createNote)
 // .put('/notes/:id', updateNote)
 // .delete('/notes/:id', deletNote);
 
